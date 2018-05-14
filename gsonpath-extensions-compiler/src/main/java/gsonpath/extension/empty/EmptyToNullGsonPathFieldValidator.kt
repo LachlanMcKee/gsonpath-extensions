1bfe542658da1397c91b404ae04aa7c4d52f9523
Lachlan McKee <lachlandevelopment@gmail.com>
2018-05-15 00:53:49 +0100

    Issue #10 Added EmptyToNull annotation support for map, collection, array

--- gsonpath-extensions-compiler/src/main/java/gsonpath/extension/empty/EmptyToNullGsonPathFieldValidator.kt
+++ gsonpath-extensions-compiler/src/main/java/gsonpath/extension/empty/EmptyToNullGsonPathFieldValidator.kt
@@ -10,6 +10,7 @@ import gsonpath.extension.addException
 import gsonpath.extension.annotation.EmptyToNull
 import javax.annotation.processing.ProcessingEnvironment
 import javax.lang.model.type.ArrayType
+import javax.lang.model.type.TypeMirror
 
 /**
  * A {@link GsonPathExtension} that supports the '@Size' annotation.
@@ -34,11 +35,18 @@ class EmptyToNullGsonPathFieldValidator : GsonPathExtension {
             } catch (e: Exception) {
                 false
             }
+        val fieldMapType: Boolean =
+            try {
+                isFieldMapType(processingEnvironment, fieldInfo.typeMirror)
+            } catch (e: Exception) {
+                false
+            }
 
         val fieldType: FieldType =
             when {
                 (fieldInfo.typeMirror is ArrayType) -> FieldType.ARRAY
                 fieldCollectionType -> FieldType.COLLECTION
+                fieldMapType -> FieldType.MAP
                 (fieldInfo.typeName == ClassName.get(String::class.java)) -> FieldType.STRING
 
                 else ->
@@ -58,6 +66,16 @@ class EmptyToNullGsonPathFieldValidator : GsonPathExtension {
             .endControlFlow()
 
         return validationBuilder.build()
+    }
+
+    private fun isFieldMapType(processingEnv: ProcessingEnvironment, typeMirror: TypeMirror): Boolean {
+        val mapTypeElement = processingEnv.elementUtils.getTypeElement(Map::class.java.name)
+        val typeUtils = processingEnv.typeUtils
+        val mapWildcardType = typeUtils.getDeclaredType(mapTypeElement,
+            typeUtils.getWildcardType(null, null),
+            typeUtils.getWildcardType(null, null))
+
+        return typeUtils.isSubtype(typeMirror, mapWildcardType)
     }
 
     /**
--- integration-test/src/main/java/gsonpath/extension/empty/EmptyModel.java
+++ integration-test/src/main/java/gsonpath/extension/empty/EmptyModel.java
@@ -3,6 +3,9 @@ package gsonpath.extension.empty;
 import gsonpath.AutoGsonAdapter;
 import gsonpath.extension.annotation.EmptyToNull;
 
+import java.util.Collection;
+import java.util.Map;
+
 public interface EmptyModel {
 
     interface BaseStringModel {
@@ -10,9 +13,36 @@ public interface EmptyModel {
         String getValue();
     }
 
+    interface BaseMapModel {
+        @EmptyToNull
+        Map<String, String> getValue();
+    }
+
+    interface BaseCollectionModel {
+        @EmptyToNull
+        Collection<String> getValue();
+    }
+
+    interface BaseArrayModel {
+        @EmptyToNull
+        String[] getValue();
+    }
+
     interface Nullable {
         @AutoGsonAdapter
         interface StringModel extends BaseStringModel {
+        }
+
+        @AutoGsonAdapter
+        interface MapModel extends BaseMapModel {
+        }
+
+        @AutoGsonAdapter
+        interface CollectionModel extends BaseCollectionModel {
+        }
+
+        @AutoGsonAdapter
+        interface ArrayModel extends BaseArrayModel {
         }
     }
 
@@ -22,6 +52,27 @@ public interface EmptyModel {
             @EmptyToNull
             @gsonpath.NonNull
             String getValue();
+        }
+
+        @AutoGsonAdapter
+        interface MapModel extends BaseMapModel {
+            @EmptyToNull
+            @gsonpath.NonNull
+            Map<String, String> getValue();
+        }
+
+        @AutoGsonAdapter
+        interface CollectionModel extends BaseCollectionModel {
+            @EmptyToNull
+            @gsonpath.NonNull
+            Collection<String> getValue();
+        }
+
+        @AutoGsonAdapter
+        interface ArrayModel extends BaseArrayModel {
+            @EmptyToNull
+            @gsonpath.NonNull
+            String[] getValue();
         }
     }
 
